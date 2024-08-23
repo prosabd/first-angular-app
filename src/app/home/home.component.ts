496c1ce3845a44cbcf944755a80367ea5d1d8c43
DrNope <pro.sabd@gmail.com>
2024-08-23 17:32:53 +0200

    feat: :tada: first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+  import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="filter by city" id="name" na
