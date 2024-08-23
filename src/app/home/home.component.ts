1d51b986b4fec94d32cc92430c1c3139ce2f1e18
DrNope <pro.sabd@gmail.com>
2024-08-23 19:49:38 +0200

    feat: :thread: add new component cli usage and searchbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-  import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
-        <input type="text" placeholder="filter by city" id="name" na
+        <input type="text" placeholder="filter by city" id="name" name="name" required>
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
+  `,
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+
+}
